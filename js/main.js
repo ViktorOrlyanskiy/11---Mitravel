a5391a798c0a680348f76c3bdc2a06f1f46cb1a7
ViktorOrlyanskiy <orlyanskiy_va@mail.ru>
2022-03-23 20:26:50 +0300

    finish no adaptive

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 'use strict'
-//превращает HTML изображение в фоновое изображении CSS
-function ibg() {
 
-    let ibg = document.querySelectorAll(".ibg");
-    for (var i = 0; i < ibg.length; i++) {
-        if (ibg[i].querySelector('img')) {
-            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-        }
-    }
-}
-
-// ibg();
+const swiper1 = document.querySelector('.swiper1'),
+    swiper2 = document.querySelector('.swiper2'),
+    burger = document.querySelector('.burger'),
+    closed = document.querySelector('.menu__close'),
+    menu = document.querySelector('.menu');
 
 
-
-let swiper = new Swiper('.slider-container', {
+let swiperSlider1 = new Swiper(swiper1, {
     centeredSlides: true,
     loop: true,
     slidesPerView: 'auto',
     spaceBetween: 105,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    navigation: {
+        nextEl: '.btn-right',
+        prevEl: '.btn-left',
+    },
 });
+let swiperSlider2 = new Swiper(swiper2, {
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 105,
+
+});
+
+burger.addEventListener('click', () => {
+    menu.classList.add('menu-visible');
+})
+closed.addEventListener('click', () => {
+    menu.classList.remove('menu-visible')
+})
